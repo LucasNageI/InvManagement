57ac391900dbcc87cc1d572b9aac858912e12c4b
LucasNagel <lucasanagel@gmail.com>
2024-11-07 06:55:55 -0300

    todo list

--- Frontend/src/screens/AddRole.jsx
+++ Frontend/src/screens/AddRole.jsx
@@ -3,7 +3,25 @@ import '../styles/AddRole.css'
 
 const AddRole = () => {
   return (
-    <div>AddRole</div>
+    <main className='addrole-container'>
+    <h1 className='h1-title'>Assign Role</h1>
+    <form>
+        <div className='login-input-container'>
+            <label htmlFor='email'>User Email:</label>
+            <input type='text' id='email' name='email' required />
+        </div>
+        <div className='login-input-container'>
+            <label htmlFor='role'>Role:</label>
+            <select id='role' name='role' required>
+                <option value='user'>User</option>
+                <option value='admin'>Admin</option>
+                <option value='employee'>Employee</option>
+
+            </select>
+        </div>
+        <button type='submit'>Assign Role</button>
+    </form>
+</main>
   )
 }
 
