9f6a0ce232fd068d12691d387b037b7bae98fce8
LucasNagel <lucasanagel@gmail.com>
2024-12-09 21:36:52 -0300

    bugfix cors middleware backend

--- Backend/src/middlewares/cors.middleware.js
+++ Backend/src/middlewares/cors.middleware.js
@@ -12,6 +12,7 @@ const customCorsMiddleware = (req, res, next) => {
     }
   
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization, X-Requested-With, Accept"
