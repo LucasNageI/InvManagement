33ccb5170be11682fae8054d084e0c8ffbe7f94f
LucasNagel <lucasanagel@gmail.com>
2024-12-09 21:29:25 -0300

    bugfix cors middleware backend

--- Backend/src/middlewares/cors.middleware.js
+++ Backend/src/middlewares/cors.middleware.js
@@ -1,22 +1,30 @@
-import ENVIRONMENT from "../config/environment.js"
-
 const customCorsMiddleware = (req, res, next) => {
-  const origin = req.headers.origin
-  const allowedOrigins = [ENVIRONMENT.FRONTEND_URL, "http://localhost:5173"]
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin)
-    res.setHeader("Access-Control-Allow-Credentials", "true")
+    const origin = req.headers.origin
+    const allowedOrigins = [
+      ENVIRONMENT.FRONTEND_URL,
+      "http://localhost:5173",
+      "https://frontend-invmanagement-itwo82fwd-lucas-nagels-projects.vercel.app",
+    ]
+  
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader("Access-Control-Allow-Origin", origin)
+      res.setHeader("Access-Control-Allow-Credentials", "true")
+    }
+  
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    )
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Requested-With"
+    )
+  
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204)
+    }
+  
+    next()
   }
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Requested-With"
-  )
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204)
-  }
-  next()
-}
-
-export default customCorsMiddleware
+  
+  export default customCorsMiddleware
